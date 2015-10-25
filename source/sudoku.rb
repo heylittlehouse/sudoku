45ecbfc78ac5fe862ebe7b05154f8e6cf1d88ab9
heylittlehouse <michael.r.landon@gmail.com>
2015-10-25 18:07:35 -0400

    Add method that organizes board into the 3x3 squares

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -2,7 +2,7 @@ class Sudoku
   def initialize(board_string)
   	#turns board into array of arrays
   	@board = board_string.scan(/.{9}/).map{|string| string.split("")}
-  	p @board
+  	to_s
   end
 
 #returns hash of missing numbers for each row.
@@ -31,11 +31,75 @@ class Sudoku
   	missing_nos_row
   end
 
-  def possibilities_each_cell
+#create a new hash that associates coordinate cell with possible answers
+  def get_possibilities_each_cell
+  	possibilities_each_cell = {}
+  	missing_rows, missing_col = missing_nos_row, missing_nos_col
+  	@board = @board.transpose #puts board back in original position after missing_nos_col call
 
+  	missing_rows.each do |row, missing_in_row|
+  		missing_col.each do |col, missing_in_col|
+  			coordinate = [row, col]
+  			possibilities_each_cell[coordinate] = missing_in_row & missing_in_col if @board[row][col] == '-'
+  		end
+  	end
+
+  	possibilities_each_cell
+  end
+
+  def won?
+  	! @board.flatten.include?('-')
+  end
+
+  def valid?(array)
+  	#need methods that create a single array for a row, column, and box
+  	checker = array.uniq
+  	array == checker
+  end
+
+  def legalize
+  	@board.each do |row|
+  		valid?(row)
+  	end
+
+  	@board.transpose.each do |col|
+  		valid?(col)
+  	end
+
+  	
+  end
+
+#returns array where each element is an array of 3x3 squares.
+  def organize_board_according_to_3x3_squares
+  	board_in_3_by_3_squares =[]
+
+  	begin_index = 0
+  	end_index = 2
+
+  	while board_in_3_by_3_squares.length < 9
+	  	@board.each do |row|
+	  		three_by_three = []
+
+	  		three_by_three << row[begin_index..end_index]
+			board_in_3_by_3_squares<<three_by_three.flatten
+	  	end
+	  	begin_index +=3
+	  	end_index +=3
+  	end
+  	board_in_3_by_3_squares
   end
 
   def solve
+  	#base cases
+  	#iterate through each row, column and box and pass to valid
+
+  	return to_s if  won?
+  	possibilities_each_cell = get_possibilities_each_cell
+
+  	possibilities_each_cell.each do |coordinate, possible_answers|
+  		row_index, col_index = coordinate[0], coordinate[1]
+
+  	end
   end
 
   def board
@@ -43,10 +107,11 @@ class Sudoku
 
   # Returns a string representing the current state of the board
   def to_s
+  	@board.each{|row| puts row.join("")}
   end
 end
 
 board = Sudoku.new('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--
 ')
 
-board.missing_nos_row
+board.get_possibilities_each_cell
