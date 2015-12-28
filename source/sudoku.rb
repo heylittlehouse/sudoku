cdc8635e8d275fa315e61d0197b662a27cce6345
heylittlehouse <michael.r.landon@gmail.com>
2015-12-27 22:13:57 -0800

    Convert board_string to nested array, impliment to_s method

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -3,7 +3,19 @@ require 'pry'
 class Sudoku
   attr_reader :board
 
-  initialize(board_string)
-    @board = board_string.splice(9)
+  def initialize(board_string)
+    @board = board_string.split("").each_slice(9).to_a
+    to_s
+  end
+
+  def to_s
+    board.each do |line|
+      puts line.join("")
+    end
+    puts "\n"
+  end
+
+  def solve
+
   end
 end
