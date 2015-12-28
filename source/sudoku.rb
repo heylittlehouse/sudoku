d019f38880789c2b05ea391ae53d137b5762173c
heylittlehouse <michael.r.landon@gmail.com>
2015-12-27 22:31:21 -0800

    Add solved? method

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -2,20 +2,28 @@ require 'pry'
 
 class Sudoku
   attr_reader :board
+  attr_accessor :solved_board
 
   def initialize(board_string)
     @board = board_string.split("").each_slice(9).to_a
-    to_s
+    @solved_board = @board
+    to_s(board)
   end
 
-  def to_s
-    board.each do |line|
+  def to_s(some_board)
+    some_board.each do |line|
       puts line.join("")
     end
     puts "\n"
   end
 
+  def solved?
+    solved_board.flatten.include?("-")
+  end
+
   def solve
+    return false if !legal_board?
+    return to_s(solved_board) if solved?
 
   end
 end
